d150d0aa02b252f3cbf49ab1b7261d3a86a5d5b4
bkgunderson <brian@bkgunderson.com>
2017-02-01 22:32:56 -0600

    change logic so that a low and high number are used for bisection search

--- numberguess.py
+++ numberguess.py
@@ -1,18 +1,19 @@
-print("Please think of a number between 0 and 100!  ")
+print("Please think of a number between 0 and 100!")
 acceptable = ['h','l','c']
-guess = 50
-guessnumber = 1
+high = 101
+low = 0
+guess = int((high + low)/2.0)
 
 while True:
     response = raw_input("Is your secret number " + str(guess) + "? \n Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.")
     if response not in acceptable:
         print("Sorry, I did not understand your input.")
     elif response == 'l':
-        guessnumber += 1
-        guess = int(guess + (100 / 2**guessnumber))
+        low = guess
+        guess = int(low + ((high - low) / 2.0))
     elif response == 'h':
-        guessnumber += 1
-        guess = int(guess - (100 / 2**guessnumber))
+        high = guess
+        guess = int(high - ((high - low) / 2.0))
     else:
         break
 
