a88df05b6c202c404a3f285c097f39ccc1fd9174
bkgunderson <brian@bkgunderson.com>
2017-02-01 21:15:27 -0600

    initial commit of number guessing script

---
+++ numberguess.py
@@ -0,0 +1,19 @@
+print("Please think of a number between 0 and 100!  ")
+acceptable = ['h','l','c']
+guess = 50
+guessnumber = 1
+
+while True:
+    response = input("Is your secret number " + str(guess) + "? \n Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.")
+    if response not in acceptable:
+        print("Sorry, I did not understand your input.")
+    elif response == 'l':
+        guessnumber += 1
+        guess = int(guess + (100 / 2**guessnumber))
+    elif response == 'h':
+        guessnumber += 1
+        guess = int(guess - (100 / 2**guessnumber))
+    else:
+        break
+
+print("Game over. Your secret number was: %r" % guess)
